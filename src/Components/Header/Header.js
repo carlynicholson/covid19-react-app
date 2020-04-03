d9936fe1fe4eb4687fcf9db83338d6877dc2d0d1
carlynicholson <carlypnicholson@gmail.com>
2020-04-03 04:15:29 -0500

    header update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,14 +5,14 @@ export default function Header() {
   return (
     <div>
     <nav className="myTopNav">
-    <div className="logo">
-    <a href="/">Logo</a> 
-    </div>
+    {/* <div className="logo">
+    <a href="/"><img src="https://res.cloudinary.com/df6sigxz7/image/upload/v1585903818/covid-tracker/logo/orange2.png" alt="Logo"/></a> 
+    </div> */}
       <ul>
         <li><a href="/about">About</a></li>
       </ul>
     </nav>
-    <div>
+    <div className="header">
       <h1><a href="/">COVID-19 Tracker</a></h1>
     </div>
     </div>
